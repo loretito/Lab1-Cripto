7adfd53616836c216896eeccbd5ed87b37447cbf
Loreto <loreto.nancucheo@gmail.com>
2024-08-25 22:01:25 -0400

    refactor: :recycle: Update readv2.py to improve ICMP message processing

--- codes/readv2.py
+++ codes/readv2.py
@@ -3,27 +3,26 @@ from scapy.all import *
 from functions import evaluate_consistency, cesar
 
 if len(sys.argv) < 2:
-    print("Uso: python3 leer_icmp.py <archivo.pcapng>")
     sys.exit(1)
 
-archivo_pcapng = sys.argv[1]
-paquetes = rdpcap(archivo_pcapng)
+file_pcapng = sys.argv[1]
+packages = rdpcap(file_pcapng)
 
-words = ""
+phrase = ""
 
-for paquete in paquetes:
-    if ICMP in paquete and paquete[IP].dst == "8.8.8.8":
-        if Raw in paquete:  
-            data = paquete[Raw].load
+for package in packages:
+    if ICMP in package and package[IP].dst == "8.8.8.8":
+        if Raw in package:  
+            data = package[Raw].load
             word = data.decode('utf-8', errors='ignore')
             if word:  # Verifica que no esté vacío
                 first_letter = word[0]
-                words+= first_letter
+                phrase+= first_letter
           
-print(f"0: \t{words}")
+print(f"0: \t{phrase}")
 
-for i in range(len(words)):
-    tmp = cesar(words, i+1, False)
+for i in range(len(phrase)):
+    tmp = cesar(phrase, i+1, False)
 
     if evaluate_consistency(tmp):
         print(f"\033[92m{i+1}:\t{tmp}\033[0m")  
